f000e007b8386d2e18c9ef53ce68a645655cee39
Cj24355 <69847805+Cj24355@users.noreply.github.com>
2023-07-03 08:18:24 +0800

    Add files via upload

---
+++ DSP.ino
@@ -0,0 +1,131 @@
+#include <WebServer.h>
+#include <WiFi.h>
+#include <esp32cam.h>
+
+const char* WIFI_SSID = "ALHN-72AD";
+const char* WIFI_PASS = "nCHLL2XrRN";
+
+WebServer server(80);
+
+// Define buzzer and LED pins
+const int BUZZER_PIN = 12;
+const int LED_PIN = 4;
+
+bool bottleDetected = false; // Declare and initialize the variable
+
+static auto loRes = esp32cam::Resolution::find(320, 240);
+static auto hiRes = esp32cam::Resolution::find(800, 600);
+static auto midRes = esp32cam::Resolution::find(350, 530);
+
+void handleBottleDetected()
+{
+  // Set the bottleDetected variable to true
+  bottleDetected = true;
+  server.send(200, "text/plain", "Bottle detected");
+}
+
+void serveJpg()
+{
+  auto frame = esp32cam::capture();
+  if (frame == nullptr) {
+    Serial.println("CAPTURE FAIL");
+    server.send(503, "", "");
+    return;
+  }
+  Serial.printf("CAPTURE OK %dx%d %db\n", frame->getWidth(), frame->getHeight(),
+                static_cast<int>(frame->size()));
+
+  server.setContentLength(frame->size());
+  server.send(200, "image/jpeg");
+  WiFiClient client = server.client();
+  frame->writeTo(client); 
+}
+
+void handleJpgLo()
+{
+  if (!esp32cam::Camera.changeResolution(loRes)) {
+    Serial.println("SET-LO-RES FAIL");
+  }
+  serveJpg();
+}
+
+void handleJpgHi()
+{
+  if (!esp32cam::Camera.changeResolution(hiRes)) {
+    Serial.println("SET-HI-RES FAIL");
+  }
+  serveJpg();
+}
+
+void handleJpgMid()
+{
+  if (!esp32cam::Camera.changeResolution(midRes)) {
+    Serial.println("SET-MID-RES FAIL");
+  }
+  serveJpg();
+}
+
+void playBuzzer()
+{
+  // Turn on the buzzer
+  Serial.println("Bottle is Detected");
+  digitalWrite(BUZZER_PIN, HIGH);
+  delay(1000); // Adjust the delay as per your requirement
+  // Turn off the buzzer
+  digitalWrite(BUZZER_PIN, LOW);
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+
+  pinMode(BUZZER_PIN, OUTPUT); // Set the buzzer pin as an output
+  pinMode(LED_PIN, OUTPUT); // Set the LED pin as an output
+
+  {
+    using namespace esp32cam;
+    Config cfg;
+    cfg.setPins(pins::AiThinker);
+    cfg.setResolution(hiRes);
+    cfg.setBufferCount(2);
+    cfg.setJpeg(80);
+
+    bool ok = Camera.begin(cfg);
+    Serial.println(ok ? "CAMERA OK" : "CAMERA FAIL");
+  }
+  WiFi.persistent(false);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+  Serial.print("http://");
+  Serial.println(WiFi.localIP());
+  Serial.println("  /cam-lo.jpg");
+  Serial.println("  /cam-hi.jpg");
+  Serial.println("  /cam-mid.jpg");
+
+  server.on("/cam-lo.jpg", handleJpgLo);
+  server.on("/cam-hi.jpg", handleJpgHi);
+  server.on("/cam-mid.jpg", handleJpgMid);
+  
+  // Add a new route to handle the notification
+  server.on("/bottle-detected", handleBottleDetected);
+
+  server.begin();
+}
+
+void loop()
+{
+  server.handleClient();
+
+  // Check for the bottle detection condition
+  if (bottleDetected) {
+    playBuzzer();
+    digitalWrite(LED_PIN, HIGH); // Turn on the LED
+    bottleDetected = false; // Reset the variable after playing the buzzer
+  } else {
+    digitalWrite(LED_PIN, LOW); // Turn off the LED
+  }
+}
